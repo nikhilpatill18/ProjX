1e2f41405fa2aefe38c9740c7828a24dec4f4532
nikhilpatill18 <nikhilpatil6060@gmail.com>
2025-07-25 22:50:43 +0530

    feat: Refactor App component structure to integrate Redux provider and improve routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,24 +10,31 @@ import Setting from './pages/Setting.jsx'
 import Userprofile from './pages/Userprofile.jsx'
 import AddProject from './pages/AddProject.jsx'
 import Projects from './pages/Projects.jsx'
+import ProjectDetailsModal from './components/ProjectDetailsModal.jsx'
+import { Provider } from 'react-redux'
+import store from './store/Store.js'
 const App = () => {
   return (
     <div>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/login' element={<Login />} />
+      <Provider store={store}>
+        <AuthProvider>
 
-            <Route path='/dashboard' element={<Protectedroute><Dashboard /></Protectedroute>} />
-            <Route path='/setting' element={<Protectedroute><Setting /></Protectedroute>} />
-            <Route path='/profile' element={<Protectedroute><Userprofile /></Protectedroute>} />
-            <Route path='/add-project' element={<Protectedroute><AddProject /></Protectedroute>} />
-            <Route path='/your-project' element={<Protectedroute><Projects /></Protectedroute>} />
+          <Router>
+            <Routes>
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/login' element={<Login />} />
 
-          </Routes>
-        </Router>
-      </AuthProvider>
+              <Route path='/dashboard' element={<Protectedroute><Dashboard /></Protectedroute>} />
+              <Route path='/setting' element={<Protectedroute><Setting /></Protectedroute>} />
+              <Route path='/profile' element={<Protectedroute><Userprofile /></Protectedroute>} />
+              <Route path='/add-project' element={<Protectedroute><AddProject /></Protectedroute>} />
+              <Route path='/your-project' element={<Protectedroute><Projects /></Protectedroute>} />
+              {/* <Route path='project/:id' element={<Protectedroute><ProjectDetailsModal /></Protectedroute>} /> */}
+
+            </Routes>
+          </Router>
+        </AuthProvider>
+      </Provider>
 
     </div>
   )
