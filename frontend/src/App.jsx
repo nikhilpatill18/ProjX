ce1f59fc178a09fef07beea94a600805da918483
nikhilpatill18 <nikhilpatil6060@gmail.com>
2025-07-31 20:15:53 +0530

    fix: correct UserProfile import and update route for MyProjectPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,10 @@ import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Check from './pages/Check.jsx'
 import Setting from './pages/Setting.jsx'
-import Userprofile from './pages/Userprofile.jsx'
+import UserProfile from './pages/Userprofile.jsx'
 import AddProject from './pages/AddProject.jsx'
 import Projects from './pages/Projects.jsx'
+import MyProjectPage from './pages/MyProjectPage.jsx'
 import ProjectDetailsModal from './components/ProjectDetailsModal.jsx'
 import { Provider } from 'react-redux'
 import store from './store/Store.js'
@@ -26,10 +27,10 @@ const App = () => {
 
               <Route path='/dashboard' element={<Protectedroute><Dashboard /></Protectedroute>} />
               <Route path='/setting' element={<Protectedroute><Setting /></Protectedroute>} />
-              <Route path='/profile' element={<Protectedroute><Userprofile /></Protectedroute>} />
+              <Route path='/profile' element={<Protectedroute><UserProfile /></Protectedroute>} />
               <Route path='/add-project' element={<Protectedroute><AddProject /></Protectedroute>} />
               <Route path='/your-project' element={<Protectedroute><Projects /></Protectedroute>} />
-              {/* <Route path='project/:id' element={<Protectedroute><ProjectDetailsModal /></Protectedroute>} /> */}
+              <Route path='/projects/:projectId' element={<Protectedroute><MyProjectPage /></Protectedroute>} />
 
             </Routes>
           </Router>
